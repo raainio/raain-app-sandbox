d53dfc97f63aaa6f7713629ee10685b9c5f587ff
BB pipeline <mlefree>
2024-11-05 19:28:13 +0000

    BB pipeline

---
+++ 2680.57008bdec5ee6590.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[2680],{2680:(_,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>a});var s=e(5861),t=e(7423);class a extends t.Uw{show(u){return(0,s.Z)(function*(){})()}hide(u){return(0,s.Z)(function*(){})()}}}}]);
